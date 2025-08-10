f5b30226280af0153ea883432f8ea2b50e83cea7
Surjeet <surjeet@Surjeets-MacBook-Air.local>
2025-08-10 14:06:03 +0530

    add render backend url

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,8 +1,8 @@
 const { default: axios } = require("axios");
 
 
-// export const BASE_URL="https://social-media-plateform-vy0x.onrender.com"
-export const BASE_URL="http://localhost:9090"
+export const BASE_URL="https://social-media-plateform-vy0x.onrender.com"
+// export const BASE_URL="http://localhost:9090"
 
 //ek instance create kar diya hai 
 const clientServer=axios.create({
